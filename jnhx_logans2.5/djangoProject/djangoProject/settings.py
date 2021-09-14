e8853ff7ea09fc8a7689bd589055913011b83c00
shanwenjie <shanwenjie1213@163.com>
2021-09-14 11:59:00 +0800

    修改setting文件

--- jnhx_logans2.5/djangoProject/djangoProject/settings.py
+++ jnhx_logans2.5/djangoProject/djangoProject/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
-
+import sys
+import os
 # import djcelery
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(os.path.join(BASE_DIR, "test_log"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
